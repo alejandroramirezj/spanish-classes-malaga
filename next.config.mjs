9bdd79b4784d2250d4b8c39da228a4cdc849906b
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-03-15 17:55:36 +0100

    Fix some bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/spanish-classes-malaga',
   images: {
     unoptimized: true,
     domains: ['localhost'],
