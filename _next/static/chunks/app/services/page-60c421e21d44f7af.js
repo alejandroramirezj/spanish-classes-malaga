ee254b3cbd43efbd44def5eac34fa23fa8a790d9
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-03-16 23:38:48 +0100

    Updates

---
+++ _next/static/chunks/app/services/page-60c421e21d44f7af.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[763],{8359:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.bind(n,7145)),Promise.resolve().then(n.bind(n,2770))}},e=>{var s=s=>e(e.s=s);e.O(0,[711,109,482,441,684,358],()=>s(8359)),_N_E=e.O()}]);
